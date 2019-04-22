4cacd3241394e37425119f2097368339d5ad92cd
Brad Osgood <bosgood@gmail.com>
2019-04-21 22:11:11 -0400

    Continuing implementation

--- pkg/opc/opc.go
+++ pkg/opc/opc.go
@@ -2,46 +2,48 @@ package opc
 
 import (
 	"io"
+	"log"
 	"math"
 	"net"
 )
 
-// Color represents an RGB value
-type Color [3]byte
+// ARGB represents an RGB value with alpha
+type ARGB uint32
 
 // Red gets the red component's value
-func (c *Color) Red() byte {
-	return c[0]
+func (a ARGB) Red() byte {
+	return byte(a >> 24 & 0xFF)
 }
 
 // Green gets the red component's value
-func (c *Color) Green() byte {
-	return c[1]
+func (a ARGB) Green() byte {
+	return byte(a >> 16 & 0xFF)
 }
 
 // Blue gets the red component's value
-func (c *Color) Blue() byte {
-	return c[2]
+func (a ARGB) Blue() byte {
+	return byte(a >> 8 & 0xFF)
 }
 
-func (c *Color) Int32() uint32 {
-	return 0
+// ARGBColor gets a new aRGB value from its component values
+func ARGBColor(a, r, g, b byte) ARGB {
+	return ARGB(a | r | g | b)
 }
 
 // OPC manages connections to an Open Pixel Control server
 type OPC struct {
-	Host                string
-	Port                int
-	Width               int
-	Height              int
-	PixelLocations      []int
-	PacketData          []byte
-	FirmwareConfig      byte
-	ColorCorrection     string
-	EnableShowLocations bool
-	Output              io.Writer
-	Connection          net.Conn
-	Pixels              []Color
+	Host            string
+	Port            int
+	Width           int
+	Height          int
+	PixelLocations  []int
+	PacketData      []byte
+	FirmwareConfig  byte
+	ColorCorrection string
+	Output          io.Writer
+	Pending         io.Writer
+	Connection      net.Conn
+	Pixels          []ARGB
 }
 
 // New creates a new OPC client
@@ -86,6 +88,7 @@ func (o *OPC) LEDStrip(index, count int, x, y, spacing, angle float64, reversed 
 	}
 }
 
+// Draw maps the pixel array to color values and sends it to the OPC server
 func (o *OPC) Draw() error {
 	if o.PixelLocations == nil || o.Connection == nil || o.Output == nil {
 		return nil
@@ -96,15 +99,24 @@ func (o *OPC) Draw() error {
 	o.SetPixelCount(numPixels)
 
 	for i := 0; i < numPixels; i++ {
-		loc := o.PixelLocations[i]
-		pixel := o.Pixels[loc]
-		o.PacketData[ledAddress] = byte(pixel >> 16)
+		pixelLocation := o.PixelLocations[i]
+		pixel := o.Pixels[pixelLocation]
+		pd := o.PacketData
+		pd[ledAddress] = byte(pixel >> 16)
+		pd[ledAddress+1] = byte(pixel >> 8)
+		pd[ledAddress+2] = byte(pixel)
+		ledAddress += 3
+	}
 
+	err := o.WritePixels()
+	if err != nil {
+		return err
 	}
 
 	return nil
 }
 
+// SetPixelCount sets the total number of pixels
 func (o *OPC) SetPixelCount(numPixels int) {
 	numBytes := 3 * numPixels
 	packetLen := 4 + numBytes
@@ -117,4 +129,39 @@ func (o *OPC) SetPixelCount(numPixels int) {
 		pd[3] = byte(numBytes & 0xFF) // Length low byte
 		o.PacketData = pd
 	}
+}
+
+// SetPixel sets the color value of a pixel
+func (o *OPC) SetPixel(number int, c ARGB) {
+	offset := 4 + number*3
+	if o.PacketData == nil || len(o.PacketData) < offset+3 {
+		o.SetPixelCount(number + 1)
+	}
+
+	pd := o.PacketData
+	pd[offset] = byte(c >> 16)
+	pd[offset+1] = byte(c >> 8)
+	pd[offset+2] = byte(c)
+}
+
+// WritePixels writes the current buffer of pixel values to the OPC server
+func (o *OPC) WritePixels() error {
+	if o.PacketData == nil || len(o.PacketData) == 0 || o.Output == nil {
+		return nil
+	}
+
+	_, err := o.Output.Write(o.PacketData)
+	return err
+}
+
+// Close disconnects from the OPC server
+func (o *OPC) Close() {
+	if o.Output != nil {
+		log.Println("Disconnecting from OPC server")
+		o.Output = nil
+	}
+	if o.Connection != nil {
+		_ = o.Connection.Close()
+	}
+	o.Pending = nil
 }
