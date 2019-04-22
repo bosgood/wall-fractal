ff4d232407970905272731bf7e6dc74cac00db6b
Brad Osgood <bosgood@gmail.com>
2019-04-21 20:29:27 -0400

    Sending the OPC message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,7 +14,7 @@ var (
 
 func parseargs() {
 	flag.StringVar(&addr, "addr", "127.0.0.1:7890", "Address and port of the OPC server to connect to")
-	flag.IntVar(&opcChannel, "channel", 0, "OPC channel to send to")
+	flag.IntVar(&opcChannel, "channel", opc.BROADCAST_CHANNEL, "OPC channel to send to")
 	flag.Parse()
 }
 
@@ -31,21 +31,14 @@ func main() {
 
 	log.Printf("Connected to server at: %s", addr)
 
-	// Make a message!
-	// This creates a message to send on channel 0
-	// Or according to the OPC spec, a Broadcast Message.
-	m := opc.NewMessage(uint8(opcChannel))
-
-	for i := 0; i < 20; i++ {
+	for i := 0; i < 50; i++ {
+		m := opc.NewMessage(uint8(opcChannel))
 		m.SetPixelColor(i, 255, 255, 255)
+		err = c.Send(m)
+		if err != nil {
+			log.Fatalf("FATAL: failed to send message: %s", err)
+		}
 	}
 
-	// Send the message!
-	err = c.Send(m)
-	if err != nil {
-		log.Fatalf("FATAL: failed to send message: %s", err)
-	}
 	log.Printf("Sent message to OPC channel %d", opcChannel)
-
-	// The first pixel of all registered devices should be white!
 }
